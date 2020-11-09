be768fb4286a295fcd6c8991665a2a17d4562b12
Mateusz <kawkamateusz@pm.me>
2020-11-09 12:17:33 +0100

    Initial dist subtree commit

---
+++ precache-manifest.4f426463b65025955c2c41d9a61acaf8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8bb1fc74ee20acca273",
+    "url": "/vue-quiz/css/app.b8e8774a.css"
+  },
+  {
+    "revision": "5a2397b422a269944faf",
+    "url": "/vue-quiz/css/chunk-18d0fef1.23e63752.css"
+  },
+  {
+    "revision": "b444e73a5781635b7126",
+    "url": "/vue-quiz/css/chunk-d15d4dbc.60d7cc32.css"
+  },
+  {
+    "revision": "76a9fd538b3642667462",
+    "url": "/vue-quiz/css/chunk-vendors.4db5c369.css"
+  },
+  {
+    "revision": "cd5a0806547bae1fc9ea",
+    "url": "/vue-quiz/css/game.c01c1e90.css"
+  },
+  {
+    "revision": "5fad700adc948cb51404d55833347f51",
+    "url": "/vue-quiz/fonts/feather.5fad700a.eot"
+  },
+  {
+    "revision": "66cbb621b431bf32041a5c478e5539c0",
+    "url": "/vue-quiz/fonts/feather.66cbb621.woff"
+  },
+  {
+    "revision": "a940fe89dbfe9d1d89fc1aa0488fe032",
+    "url": "/vue-quiz/fonts/feather.a940fe89.ttf"
+  },
+  {
+    "revision": "023ba0824a99109c4a63622228dd6354",
+    "url": "/vue-quiz/img/feather.023ba082.svg"
+  },
+  {
+    "revision": "096aae9d9131021323c056cad24a6803",
+    "url": "/vue-quiz/index.html"
+  },
+  {
+    "revision": "d8bb1fc74ee20acca273",
+    "url": "/vue-quiz/js/app.0bbcedf6.js"
+  },
+  {
+    "revision": "5a2397b422a269944faf",
+    "url": "/vue-quiz/js/chunk-18d0fef1.4aa4cb8b.js"
+  },
+  {
+    "revision": "b444e73a5781635b7126",
+    "url": "/vue-quiz/js/chunk-d15d4dbc.7b0e6c91.js"
+  },
+  {
+    "revision": "76a9fd538b3642667462",
+    "url": "/vue-quiz/js/chunk-vendors.e250020d.js"
+  },
+  {
+    "revision": "cd5a0806547bae1fc9ea",
+    "url": "/vue-quiz/js/game.bb1e5423.js"
+  },
+  {
+    "revision": "3fdef390786d44aef1f5c613d4cc99d5",
+    "url": "/vue-quiz/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-quiz/robots.txt"
+  }
+]);
